e3f53a8657a1bdc4037a602b2a7c302a81aaecd3
Yoander Napoles Zaldivar <90664209+yoandernap@users.noreply.github.com>
2026-02-08 23:24:01 -0500

    Create requirements.R file with necessary packages

--- requirements.R
+++ requirements.R
@@ -1,6 +1,29 @@
-# R package dependencies
+# Required Packages
 
-dplyr
-ggplot2
-caret
-randomForest
+Packages needed for the project:
+
+- base64enc
+- abind
+- rms
+- tidyverse
+- tableone
+- pROC
+- dcurves
+- riskRegression
+- caret
+- gt
+- kableExtra
+- knitr
+- scales
+- formattable
+- rmarkdown
+- webshot
+- readxl
+- openxlsx
+- survival
+- survminer
+- ggpubr
+- corrplot
+- flextable
+- officer
+- askpass
